c9d55a219444d96bb5a9cea69fc9330d82a67878
helmi <helmitira@yahoo.com>
2019-03-29 20:28:54 -0400

    adding

--- server.js
+++ server.js
@@ -12,6 +12,34 @@ let Artice = require('./models/Articles');
 mongoose.Promise = Promise;
 let app = express();
 let PORT = process.env.PORT || 8080;
+// Use morgan and body parser //
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+ extended: false
+}));
+// Serve static content //
+app.use(express.static('public'));
+// Set Handlebars //
+let exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: 'handlebars'}));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them //
+let routes = require('./controllers/scraperController.js');
+app.use('/', routes);
+mongoose.connect('mongodb://heroku');
+let db = mongoose.connection;
+
+// Show any mongoose errors //
+db.on('error', function(error) {
+    console.log('Mongoose Error: ', error);
+  });
+// Once logged in to the db through mongoose, log a success message //
+   db.once("open", function() {
+   console.log("Mongoose connection is successful.");
+  });
+
+
 //listen on port 8080
 app.listen(PORT, function(){
     console.log('App running on port ' + PORT + '!');
